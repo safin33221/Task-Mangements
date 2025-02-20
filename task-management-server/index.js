97a3948f024987452cac362e7625d170f096ee2e
safin33221 <saifn33221@gmail.com>
2025-02-20 19:48:11 +0600

    initialize server setup completed

---
+++ task-management-server/index.js
@@ -0,0 +1,52 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const port = process.env.PORT || 5050
+
+app.use(cors())
+app.use(express.json())
+
+
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(process.env.uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        // await client.connect();
+
+        const db = client.db('Task-management')
+        const userCollection = db.collection('users')
+
+
+        
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+app.get('/', async (req, res) => {
+    res.send('Task Management server is running')
+})
+
+app.listen(port, () => {
+    console.log("server running on", port);
+})
